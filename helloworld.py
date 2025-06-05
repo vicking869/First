cb268616901e959ca360fc767afd013ef5150843
Victor Zheng <victor.zheng29@gmail.com>
2025-06-05 16:15:55 +1000

    Update message in helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,10 @@
-name = input("What is your name? ")
-print("Hello, " + name + "!")
+epsilon = 0.01
+y = 54.0
+guess = y/2.0
+numGuesses = 0
+
+while abs(guess ** 2 - y) >= epsilon:
+    numGuesses += 1
+    guess = guess - (((guess ** 2) - y)/(2*guess))
+print(f"The number of guesses it took was {numGuesses}")
+print(f"The square root of {y} is approx. {guess}")
